9ea4a79850a45fc9cdf519e8d15f54df28b765ab
G120L010 <w25961941w@gmail.com>
2026-03-16 11:44:41 +0800

    Create 51_101_while_51_101.java

---
+++ 0316/51_101_while_51_101.java
@@ -0,0 +1,19 @@
+package helloworld;
+
+public class while_51_101 {
+
+	public static void main(String[] args) {
+		//輸入(沒有)
+		int total = 0;//輸出
+		int i = 1;//暫時儲存的當前值
+		while (i <= 100) {
+			total += i;
+			System.out.println("i=" + i);
+			System.out.println("total=" + total);
+			i++;
+		}
+		System.out.println(total);
+
+	}
+
+}
