17c36ff1dd87e2416e6a3953af93c195a4abeef9
G120L010 <w25961941w@gmail.com>
2026-03-16 10:27:01 +0800

    Update 46_91_arr.java

--- 0316/46_91_arr.java
+++ 0316/46_91_arr.java
@@ -5,11 +5,10 @@ public class Module4 {
 	public static void main(String[] args) {
 		int[] arr0 = new int[5];//裡面有5筆資料
 		double[] arr1 = new double[5];//結果為0.0
-		boolean[] arr2 = new boolean[0];//結果為false
+		boolean[] arr2 = new boolean[1];//結果為false
 		System.out.println(arr0[4]);
 		System.out.println(arr1[0]);
-		System.out.println(arr2);
-		
+		System.out.println(arr2[0]);
 		
 		int[] arr = {1,2,3,4};//"int[]"不是基本資料型別，是陣列
 		System.out.println("before："+arr[0]);//陣列[]索引值順序從0開始
