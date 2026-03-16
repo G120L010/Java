51f4183c2741ae16f06c60035588c76a08a9407f
G120L010 <w25961941w@gmail.com>
2026-03-16 17:20:36 +0800

    Update 49_97_double_loop.java

--- Beginner/49_97_double_loop.java
+++ Beginner/49_97_double_loop.java
@@ -4,13 +4,28 @@ public class Module5_1 {
 
 	public static void main(String[] args) {
 //		int i,j;
-		for(int i=1; i<10 ; i++) {//從1+到9
+		
+		// 外層迴圈：控制「列」(被乘數)
+		// i 從 1 到 9
+		for(int i=1; i<10 ; i++) {
+			
+			// 內層迴圈：控制「欄」(乘數)
+			// j 從 1 到 9
 			for(int j=1; j<10; j++) {//從內層迴圈開始
 				System.out.print(j+"X" + i+"=" +(i*j) +"\t" );
-			}//1x1、2x1、3x1....
+			}
+//			for(外層){
+//			    for(內層){
+//			        執行程式
+//			    }
+//			}
+//			外層1次
+//			  內層跑完全部
+//			外層第2次
+//			  內層再跑完
+//			外層 → 控制列
+//			內層 → 控制欄
 			System.out.println();
 		}
-
 	}
-
 }
