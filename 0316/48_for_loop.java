8f6b1c2da0d7e86c3f850d4b8e9e9364a41925bf
G120L010 <w25961941w@gmail.com>
2026-03-16 10:13:58 +0800

    Create 48_for_loop.java

---
+++ 0316/48_for_loop.java
@@ -0,0 +1,19 @@
+package helloworld;
+
+public class Module5 {
+
+	public static void main(String[] args) {
+		//輸入(沒有)
+		int total = 0;//輸出
+		
+		for (int i = 1; //=寫法 (int i=1; i<=100; i++)
+				i <=100;
+				    ++i) {
+			total=total+i;
+			System.out.println("i="+i);
+			System.out.println("total="+total);
+		}
+		
+	}
+
+}
