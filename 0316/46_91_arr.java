65f3406e2c5612f9c83aa7820c33d40da94cc210
G120L010 <w25961941w@gmail.com>
2026-03-16 13:19:18 +0800

    Update 46_91_arr.java

--- 0316/46_91_arr.java
+++ 0316/46_91_arr.java
@@ -1,20 +1,51 @@
 package helloworld;
 
-public class Module4 {
+public class arr {
 
 	public static void main(String[] args) {
-		int[] arr0 = new int[5];//裡面有5筆資料
-		double[] arr1 = new double[5];//結果為0.0
-		boolean[] arr2 = new boolean[1];//結果為false
+
+		int[] arr0 = new int[5];
+		// 建立一個 int 陣列，長度為 5
+		// 裡面有 5 個元素，預設值都是 0
+
+		double[] arr1 = new double[5];
+		// 建立 double 陣列，長度為 5
+		// 預設值都是 0.0
+
+		boolean[] arr2 = new boolean[1];
+		// 建立 boolean 陣列，長度為 1
+		// 預設值為 false
+
 		System.out.println(arr0[4]);
+		// 取得 arr0 第5個元素 (index=4)
+		// int 陣列預設值為 0
+
 		System.out.println(arr1[0]);
+		// 取得 arr1 第1個元素 (index=0)
+		// double 陣列預設值為 0.0
+
 		System.out.println(arr2[0]);
-		
-		int[] arr = {1,2,3,4};//"int[]"不是基本資料型別，是陣列
-		System.out.println("before："+arr[0]);//陣列[]索引值順序從0開始
-		arr[0] = 50;//將陣列[]索引值0的位置用50的資料取代
-		System.out.println("after："+arr[0]);//顯示結果為50
-		
-		System.out.println(arr.length);//length為屬性(field)【藍色字體通常都是屬性】
+		// 取得 arr2 第1個元素
+		// boolean 陣列預設值為 false
+
+		int[] arr = { 1, 2, 3, 4 };
+		// 宣告並初始化陣列
+		// int[] 代表「整數陣列型別」
+		// 陣列本身不是基本型別，而是「參考型別 (Reference Type)」
+
+		System.out.println("before：" + arr[0]);
+		// 顯示 arr 第1個元素
+		// 陣列索引(index)從 0 開始
+
+		arr[0] = 50;
+		// 將 index=0 的值從 1 改為 50
+
+		System.out.println("after：" + arr[0]);
+		// 再次印出 arr[0]，結果會變成 50
+
+		System.out.println(arr.length);
+		// length (長度) 是陣列的「屬性 (field)」
+		// 用來取得陣列長度
+		// 這個陣列有 4 個元素，所以結果是 4
 	}
 }
