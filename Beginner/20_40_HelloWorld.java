1ed4f8829634df06c0631bfa7170899e200f9994
G120L010 <w25961941w@gmail.com>
2026-03-17 12:25:21 +0800

    Update 20_40_HelloWorld.java

--- Beginner/20_40_HelloWorld.java
+++ Beginner/20_40_HelloWorld.java
@@ -1,14 +1,24 @@
 package helloworld;
+
 /* 在專案中的類別HelloWorld建立人生首支程式
  * 功能：在主控台輸出 "Hello World!!" */
 public class HelloWorld {
 
-	/**
-	 * 主程式進入點 
-	 * @param args 命令列參數 */
+	/** 主程式進入點
+	    @param args 命令列參數 */
 	public static void main(String[] args) {
 
-		// 在主控台(Console)輸出文字
+		// Java 變數命名規則
+		// 一. 可用：英文字母、數字、_、$、Unicode(如中文)
+		// 二. 不可：數字開頭、Java 關鍵字
+		// 三. ; 為程式敘述結束
+		// 四. 多單字使用駝峰式命名 camelCase（第一字小寫，其後單字開頭大寫）
+		int $_idAge年齡 = 18; // 命名規則測試
+		int userAge = 20; // camelCase 範例
+
+		// 在主控台(Console)輸出程式運行結果
+		System.out.println($_idAge年齡);
+		System.out.println(userAge);
 		System.out.println("Hello World!!");
 
 		// ===== Eclipse 常用快捷鍵 =====
