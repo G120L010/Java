9b5ef3a3a1044745bd9d85150fde758b5f28f340
G120L010 <w25961941w@gmail.com>
2026-03-18 16:49:42 +0800

    Update 18_35_Demo.java

--- Advanced/18_35_Demo.java
+++ Advanced/18_35_Demo.java
@@ -1,14 +1,23 @@
 package advanced;
-
+/*Demo 類別：提供基本的加法與字串列印功能*/
 public class Demo {
-	//兩數相加
-	public int add(int a,int b) {
-		int c=a+b;
-		return c;
-	}
-	//列印字串
-	public void print(String str) {
-		System.out.println(str);
-//		return;可寫可不寫，因為都會回傳
-	}
+    /**
+     * 兩數相加的方法
+     * @param a 第一個整數
+     * @param b 第二個整數
+     * @return 回傳 a + b 的結果
+     */
+    public int add(int a, int b) {
+        int c = a + b; // 將兩數相加並存入變數 c
+        return c;      // 回傳加總結果
+    }
+    /** 列印字串到主控台
+     * @param str 要被列印的字串
+     */
+    public void print(String str) {
+        System.out.println(str); // 將字串輸出到控制台
+
+        // return; 可寫可不寫
+        // 因為 void 方法在結束時會自動返回
+    }
 }
