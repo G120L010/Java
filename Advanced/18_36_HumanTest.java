812a5cc5eb27865806f2b0cc022b949447f454f9
G120L010 <w25961941w@gmail.com>
2026-03-18 16:33:51 +0800

    Update 18_36_HumanTest.java

--- Advanced/18_36_HumanTest.java
+++ Advanced/18_36_HumanTest.java
@@ -4,10 +4,14 @@ public class HumanTest {
 
 	public static void main(String[] args) {
 		Human h = new Human();
-		h.height=1.75;
-		h.weight=50;
+		h.height = 1.75;
+		h.weight = 50;
 		double bmi = h.getBMI();
 		System.out.println(bmi);
+
+		// 靜態方法的呼叫使用
+		double bmi2 = Human.getBMI2(50, 1.75);
+		System.out.println(bmi2);
 	}
 
 }
