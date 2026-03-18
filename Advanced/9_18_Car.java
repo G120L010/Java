a87dd113090161bf2457e79ec94577a0d7c05e20
G120L010 <w25961941w@gmail.com>
2026-03-18 11:05:48 +0800

    Update and rename 8_15_Type.java to 9_18_Car.java

---
+++ Advanced/9_18_Car.java
@@ -0,0 +1,12 @@
+package advanced;
+//類別：理論上沒有實體，去訂定結構
+//物件：架構與行為不會改變
+
+public class Car {			//類別
+	public String brand;	//屬性
+	public String color;	//屬性
+	
+	public void go() {		//method方法
+		System.out.println("出發");
+	}
+}
