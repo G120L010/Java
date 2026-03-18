39efbbc15644d30780a145ccd53915c9dda90af9
G120L010 <w25961941w@gmail.com>
2026-03-18 16:01:15 +0800

    Create 18_35_Demo.java

---
+++ Advanced/18_35_Demo.java
@@ -0,0 +1,14 @@
+package advanced;
+
+public class Demo {
+	//兩數相加
+	public int add(int a,int b) {
+		int c=a+b;
+		return c;
+	}
+	//列印字串
+	public void print(String str) {
+		System.out.println(str);
+//		return;可寫可不寫，因為都會回傳
+	}
+}
