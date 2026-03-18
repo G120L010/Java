28e97561abd2672ca8c00db6895f4dfd6de5a7c7
G120L010 <w25961941w@gmail.com>
2026-03-18 12:59:46 +0800

    Create 11_22_Obj.java

---
+++ Advanced/11_22_Obj.java
@@ -0,0 +1,26 @@
+package advanced;
+
+public class Obj {//可以使用自訂類別
+
+	public static void main(String[] args) {
+		ObjectTest o1 = new ObjectTest();
+		// 建立一個新的物件，假設記憶體位址是 0x100
+		// o1 變數存放的是 0x100
+
+		ObjectTest o2 = new ObjectTest();
+		// 再建立另一個新的物件，假設記憶體位址是 0x200
+		// o2 變數存放的是 0x200
+
+		ObjectTest o3 = o1;
+		// o3 指向和 o1 相同的物件，所以 o3 也存放 0x100
+
+		System.out.println(o1 == o2);
+		// 比較 o1 和 o2 的位址：0x100 vs 0x200 → false
+
+		System.out.println(o2 == o3);
+		// 比較 o2 和 o3 的位址：0x200 vs 0x100 → false
+
+		System.out.println(o1 == o3);
+		// 比較 o1 和 o3 的位址：0x100 vs 0x100 → true
+	}
+}
