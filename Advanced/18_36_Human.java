1e0d4a3c4c15f32ae143b3fe4cbff73a3542d06d
G120L010 <w25961941w@gmail.com>
2026-03-18 16:34:20 +0800

    Update 18_36_Human.java

--- Advanced/18_36_Human.java
+++ Advanced/18_36_Human.java
@@ -1,13 +1,19 @@
 package advanced;
 
 public class Human {
-	//建立屬性
+	// 建立屬性
 	double height;
 	double weight;
-	
-	//建立方法
+
+	// 建立方法
 	public double getBMI() {
-		double bmi=weight/Math.pow(height, 2);
+		double bmi = weight / Math.pow(height, 2);
+		return bmi;
+	}
+
+	// 靜態方法
+	public static double getBMI2(double weight, double height) {
+		double bmi = weight / Math.pow(height, 2);
 		return bmi;
 	}
 
