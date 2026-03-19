45cb1a9ee795cea3118aa299c73796f36561c1a6
G120L010 <w25961941w@gmail.com>
2026-03-19 15:49:03 +0800

    Create 34_68_BStatic.java

---
+++ Advanced/34_68_BStatic.java
@@ -0,0 +1,15 @@
+package a.b;
+
+import a.a.AStatic;
+
+public class BStatic {
+
+	public static void main(String[] args) {
+		Afinal a = new Afinal();
+		Afinal a1 = new Afinal();
+		Afinal a2 = new Afinal();
+		Afinal a3 = new Afinal();
+		System.out.println();
+		
+	}
+}
