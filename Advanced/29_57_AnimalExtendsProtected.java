4acc9054cb9f5e464156762edb712f552b555069
G120L010 <w25961941w@gmail.com>
2026-03-19 12:05:10 +0800

    Create 29_57_AnimalExtendsProtected.java

---
+++ Advanced/29_57_AnimalExtendsProtected.java
@@ -0,0 +1,16 @@
+package advanced;
+//父類別
+public class AnimalExtendsProtected {
+	// 建構方法
+	private String name;
+	// 修飾子
+	protected String name2;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
