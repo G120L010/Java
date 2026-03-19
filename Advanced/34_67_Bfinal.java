fa677cb44de6db3e8bc1f435382b8e85d0187b3b
G120L010 <w25961941w@gmail.com>
2026-03-19 15:30:02 +0800

    Update 34_67_Bfinal.java

--- Advanced/34_67_Bfinal.java
+++ Advanced/34_67_Bfinal.java
@@ -2,9 +2,9 @@ package a.b;
 
 import a.a.Afinal;
 
-public class Bfinal extends Afinal { // 觀察3
-	public void name() { // 觀察2
-		ib = 100; // 觀察1
+public class Bfinal extends Afinal { // 觀察3：有無final的差別
+	public void name() { // 觀察2：有無final的差別
+		ib = 100; // 觀察1：有無final的差別
 	}
 
 }
