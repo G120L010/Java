749a498db48c1fd5f9bf0d1e53b94d10cbc60c54
G120L010 <w25961941w@gmail.com>
2026-03-19 11:45:26 +0800

    Create 29_57_TestExtends.java

---
+++ Advanced/29_57_TestExtends.java
@@ -0,0 +1,12 @@
+package advanced;
+
+public class TestExtends {
+
+	public static void main(String[] args) {
+		HumanExtends h = new HumanExtends();
+		h.setName("猴子");
+		System.out.println(h.getName());
+		h.eat();
+
+	}
+}
