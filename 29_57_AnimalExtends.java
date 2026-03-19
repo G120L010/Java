b5dc3700ffe1635210e879371e5a1c82981fa219
G120L010 <w25961941w@gmail.com>
2026-03-19 11:43:21 +0800

    Create 29_57_AnimalExtends.java

---
+++ 29_57_AnimalExtends.java
@@ -0,0 +1,14 @@
+package advanced;
+//父類別
+public class AnimalExtends {
+	//建構方法
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
