5c565bf8ed0e5d379a1e5f340b663f1c19996713
G120L010 <w25961941w@gmail.com>
2026-03-19 15:29:45 +0800

    Update 34_67_Afinal.java

--- Advanced/34_67_Afinal.java
+++ Advanced/34_67_Afinal.java
@@ -1,12 +1,12 @@
 package a.a;
 
-public final class Afinal { // 觀察3
+public final class Afinal { // 觀察3.：有無final的差別
 	public 			int ia = 40;
-	protected final int ib = 30; // 觀察1
+	protected final int ib = 30; // 觀察1：有無final的差別
 					int ic = 20;
 	private 	    int id = 10;
 
-	public final void name() { // 觀察2
+	public final void name() { // 觀察2：有無final的差別
 		System.out.println(ia);
 		System.out.println(ib);
 		System.out.println(ic);
