890207d890b08d714a28e2c6fba63ddad80ef822
G120L010 <w25961941w@gmail.com>
2026-03-19 15:18:17 +0800

    Create 34_67_Afinal.java

---
+++ Advanced/34_67_Afinal.java
@@ -0,0 +1,15 @@
+package a.a;
+
+public final class Afinal { // 觀察3
+	public 			    int ia = 40;
+	protected final int ib = 30; // 觀察1
+					        int ic = 20;
+	private 	    	int id = 10;
+
+	public final void name() { // 觀察2
+		System.out.println(ia);
+		System.out.println(ib);
+		System.out.println(ic);
+		System.out.println(id);
+	}
+}
