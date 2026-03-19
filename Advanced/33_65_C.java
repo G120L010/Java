7ad9f9b2610e67671835bb7c616cba179c4eded6
G120L010 <w25961941w@gmail.com>
2026-03-19 14:57:26 +0800

    Create 33_65_C.java

---
+++ Advanced/33_65_C.java
@@ -0,0 +1,13 @@
+package a.b;
+
+import a.a.A;
+
+public class C extends A {
+	public void name() {
+		System.out.println(ia);
+		System.out.println(ib);
+		System.out.println(ic);
+		System.out.println(id);
+	}
+
+}
