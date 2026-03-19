475e941ed4761f053056c46e079bdd3b7383ee69
G120L010 <w25961941w@gmail.com>
2026-03-19 10:21:39 +0800

    Create 24_48_ClassHumanTest.java

---
+++ Advanced/24_48_ClassHumanTest.java
@@ -0,0 +1,10 @@
+package advanced;
+
+public class ClassHumanTest {
+
+	public static void main(String[] args) {
+		ClassHuman h = new ClassHuman();
+		h.setHeight(175);
+		System.out.println(h.getHeight());
+	}
+}
