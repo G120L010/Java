d83066e9b136bf3f1b5c3630ad07607853882052
G120L010 <w25961941w@gmail.com>
2026-03-19 14:53:44 +0800

    Create 33_65_A.java

---
+++ Advanced/33_65_A.java
@@ -0,0 +1,15 @@
+package a.a;
+
+public class A {
+	public       int ia = 40;
+	protected    int ib = 30;
+	             int ic = 20;
+	private      int id = 10;
+	
+	public void name() {
+		System.out.println(ia);
+		System.out.println(ib);
+		System.out.println(ic);
+		System.out.println(id);
+	}
+}
