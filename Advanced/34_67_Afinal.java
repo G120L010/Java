310a3f2c1214d91f5eb773cb31061619ba1a80f7
G120L010 <w25961941w@gmail.com>
2026-03-19 15:29:07 +0800

    Update 34_67_Afinal.java

--- Advanced/34_67_Afinal.java
+++ Advanced/34_67_Afinal.java
@@ -1,10 +1,10 @@
 package a.a;
 
 public final class Afinal { // 觀察3
-	public 			    int ia = 40;
+	public 			int ia = 40;
 	protected final int ib = 30; // 觀察1
-					        int ic = 20;
-	private 	    	int id = 10;
+					int ic = 20;
+	private 	    int id = 10;
 
 	public final void name() { // 觀察2
 		System.out.println(ia);
