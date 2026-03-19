7e2caeac7372343cbef30dbb2fef0db45077b0b3
G120L010 <w25961941w@gmail.com>
2026-03-19 15:48:38 +0800

    Create 34_68_AStatic.java

---
+++ Advanced/34_68_AStatic.java
@@ -0,0 +1,18 @@
+package a.a;
+
+public class AStatic {
+	
+	public static int i =50;//靜態
+	
+	public static final int ia = 40;//靜態
+	protected 		int ib = 30;
+					int ic = 20;
+	private 	    int id = 10;
+
+	public void name() {
+		System.out.println(ia);
+		System.out.println(ib);
+		System.out.println(ic);
+		System.out.println(id);
+	}
+}
