174d58fcf672ddad5df7049636d2ce92722250de
G120L010 <w25961941w@gmail.com>
2026-03-19 14:58:04 +0800

    Create 33_65_D.java

---
+++ Advanced/33_65_D.java
@@ -0,0 +1,14 @@
+package a.b;
+
+import a.a.A;
+
+public class D {
+	public static void main(String[] args) {
+		A a = new A();
+		System.out.println(a.ia);
+		System.out.println(a.ib);
+		System.out.println(a.ic);
+		System.out.println(a.id);
+	}
+
+}
