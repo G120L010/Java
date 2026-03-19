c0367d3e898e8f6fa754813b4201cccbed849af6
G120L010 <w25961941w@gmail.com>
2026-03-19 14:56:11 +0800

    Create 33_65_B.java

---
+++ Advanced/33_65_B.java
@@ -0,0 +1,11 @@
+package a.a;
+
+public class B {
+	public static void name() {
+		A a = new A();
+		System.out.println(a.ia);
+		System.out.println(a.ib);
+		System.out.println(a.ic);
+		System.out.println(a.id);
+	}
+}
