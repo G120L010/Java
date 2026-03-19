e6ded41a5fb9b3184301f9014931fc7b2c8802ee
G120L010 <w25961941w@gmail.com>
2026-03-19 09:35:47 +0800

    Create 23_46_Private.java

---
+++ Advanced/23_46_Private.java
@@ -0,0 +1,12 @@
+package advanced;
+
+public class Private {		//類別
+//	物件私有化：封裝
+	private String brand;	//屬性
+	private String color;	//屬性
+	
+	public void go() {		//method方法
+		System.out.println("出發");
+	}
+	
+}
