9681671cafb05920413575c92241da054091be52
G120L010 <w25961941w@gmail.com>
2026-03-19 09:53:17 +0800

    Update 23_46_Private.java

--- Advanced/23_46_Private.java
+++ Advanced/23_46_Private.java
@@ -1,12 +1,32 @@
 package advanced;
 
-public class Private {		//類別
-//	物件私有化：封裝
-	private String brand;	//屬性
-	private String color;	//屬性
-	
-	public void go() {		//method方法
+public class Private { // 類別
+	private String brand; // 屬性
+	private String color; // 屬性
+
+// 使用 private 封裝後，需透過 getter / setter 存取屬性
+// 產生方式（以 Eclipse 為例）：
+//1.在{}內空白處右鍵
+//2.選Source
+//3.generate getters and setters 
+
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+		// this=>生成物件時=>this表示這個物件本身
+	}
+
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+		// this=>生成物件時=>this表示這個物件本身
+	}
+
+	public void go() { // method方法
 		System.out.println("出發");
 	}
-	
 }
