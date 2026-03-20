965f341244a17f7d6ffec4f299d5ca9c6eac4014
G120L010 <w25961941w@gmail.com>
2026-03-20 11:13:06 +0800

    Create 38_76_Fish.java

---
+++ Advanced/38_76_Fish.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Fish extends Animal implements Swimmer {
+	
+	public void 呼吸() {
+		System.out.println("用鰓呼吸");
+	}
+	public void swim() {
+		System.out.println("游泳");
+	}
+	
+
+}
