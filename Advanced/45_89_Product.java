4853371fd35e289f32ca8799967bafc199975a1e
G120L010 <w25961941w@gmail.com>
2026-03-20 12:02:21 +0800

    Create 45_89_Product.java

---
+++ Advanced/45_89_Product.java
@@ -0,0 +1,63 @@
+package demo2;
+
+import java.math.BigDecimal;
+//equals與hashCode的建立
+public class Product {//值類型物件，VO、POJO、DTO
+	private Integer id;//產品編號
+	private String name;//產品名稱
+	private BigDecimal price;
+	private Integer qty;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Product))
+			return false;
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	public Integer getQty() {
+		return qty;
+	}
+	public void setQty(Integer qty) {
+		this.qty = qty;
+	}
+
+}
