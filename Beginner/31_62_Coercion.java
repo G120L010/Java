ac1861c33be93abeb5307504285f58a28c3544b9
G120L010 <w25961941w@gmail.com>
2026-03-21 10:46:23 +0800

    Update 31_62_Coercion.java

--- Beginner/31_62_Coercion.java
+++ Beginner/31_62_Coercion.java
@@ -1,6 +1,16 @@
 package helloworld;
 
 public class Coercion {
+//	當等號左邊的範圍>(大於)右邊的範圍，則不用強制轉型，可直接放值，如：
+//	double d = 1;//1是int
+//
+//	當左邊的數值範圍<(小於)右邊的數值範圍時，則需要強制轉型，強制轉型方式，如：
+//	int i = 1;
+//	short j = (short)i;
+//
+//	例外：
+//	short、byte直接賦值的時候，可以不用在前方加入型別，如：
+//	short s = 1;
 
 	public static void main(String[] args) {
 
@@ -13,22 +23,22 @@ public class Coercion {
 		// int → byte 強制轉型
 		// byte 範圍 -128 ~ 127
 		byte b2 = (byte) i;
-		System.out.println("byte b2 = " + b2); // i 的數值超出 byte 可表示範圍 
+		System.out.println("byte b2 = " + b2); // i 的數值超出 byte 可表示範圍
 		// 強制轉型流程：
 		// 2147483648L → (int) → -2147483648 → (byte) → 0
-		//__________________________________________________________________
-		
+		// __________________________________________________________________
+
 		// 自動轉型（小轉大）
 		// int → long 自動轉型
 		int a = 100;
-		long l = a;   // 不需要寫 (long)
+		long l = a; // 不需要寫 (long)
 		System.out.println("long L = " + l);
 
 		// 自動轉型流程：
 		// 100 (int) → 自動轉成 long → 100L
 		// int → double 自動轉型
 		int x = 50;
-		double d = x;  // 不需要寫 (double)
+		double d = x; // 不需要寫 (double)
 		System.out.println("double D = " + d);
 
 		// 自動轉型流程：
