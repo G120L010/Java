bb12a87582736edfdf2b33b352ad23f1edab39af
G120L010 <w25961941w@gmail.com>
2026-03-21 14:07:20 +0800

    Create 34_68_TernaryOperator.java

---
+++ Beginner/34_68_TernaryOperator.java
@@ -0,0 +1,34 @@
+package helloworld;
+/**三元運算子觀念筆記】
+ * 1. 三元運算子根據場合會有不同答案。
+ * 2. 在邏輯上，三元運算子跟 if-else 不一樣（三元是「表達式」，必須回傳一個值）。
+ * 3. 在行為或結果上，三元運算子跟 if-else 一樣（都能達到條件分支的效果）。     */
+public class TernaryOperator {
+	public static void main(String[] args) {
+
+		// 這一行是「巢狀」三元運算子，建議新手閱讀時加上括號 ( ) 會更清楚：
+		// 原始程式：true ? false ? "A" : "B" : "C"
+		System.out.println(true ? (false ? "A" : "B") : "C");
+
+		/* --- 邏輯拆解：由外而內，由第一層 而"向內"到 第二層 ---
+		 * 口訣：問號 (?) = if，冒號 (:) = else
+		 * * 步驟 1：第一個條件是 true → 進入第一層的「肯定區」。
+		 * 此時最後方的 : "C" (第一層 else) 直接作廢不看。
+		 * * 步驟 2：剩下的部分是 false ? "A" : "B"
+		 * 第二個條件是 false → 進入第二層的「否定區」。
+		 * * 步驟 3：前面的 "A" (第二層 if) 作廢，最終答案就是 "B"。   */
+		System.out.println("---------- 對照 if-else 版本 ----------");
+		// 用 if-else 還原真相 (層級完全對應)
+		if (true) {                    // 第一層【  ?對應(if) 】
+			if (false) {               // 第二層【  ?對應(if) 】
+				System.out.println("A");
+			} else {                   // 第二層【  :對應(else) 】
+				System.out.println("B"); // 最終執行這裡！
+			}
+		} else {                       // 第一層【  :對應(else) 】
+			System.out.println("C");
+		}
+		
+		
+	}
+}
