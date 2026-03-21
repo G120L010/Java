1537ade6f71c2147b4ca862f6be0cd68305a9075
G120L010 <w25961941w@gmail.com>
2026-03-21 14:36:08 +0800

    Update 34_68_TernaryOperator.java

--- Beginner/34_68_TernaryOperator.java
+++ Beginner/34_68_TernaryOperator.java
@@ -6,9 +6,12 @@ package helloworld;
 public class TernaryOperator {
 	public static void main(String[] args) {
 
-		// 這一行是「巢狀」三元運算子，建議新手閱讀時加上括號 ( ) 會更清楚：
 		// 原始程式：true ? false ? "A" : "B" : "C"
-		System.out.println(true ? (false ? "A" : "B") : "C");
+		String x = false ? false ? "A" : "B" : "C" ;
+		System.out.println(x);
+		
+		// 這一行是「巢狀」三元運算子，建議新手閱讀時加上括號 ( ) 會更清楚：
+		System.out.println(true ? (true ? "A" : "B") : "C");
 
 		/* --- 邏輯拆解：由外而內，由第一層 而"向內"到 第二層 ---
 		 * 口訣：問號 (?) = if，冒號 (:) = else
@@ -29,6 +32,17 @@ public class TernaryOperator {
 			System.out.println("C"); //如果將第一層條件改成 false ，答案就會是 "C"。
 		}
 		
+		// --- 麥當勞點餐 ---店員問你兩個問題：1.要升級漢堡套餐嗎? 2.飲料要加大嗎?
+		// 語法：[要升級漢堡套餐嗎?] ? ([要大杯嗎?] ? "大杯綠茶" : "中杯綠茶") : "單點漢堡"
+		// 
+		// 1. 第一層條件 (true = 要升級漢堡套餐)
+		//		    -> 恭喜！你進入了套餐的選擇區，"單點漢堡" (C) 直接出局。
+		// 
+		// 2. 第二層條件 (false = 套餐飲料要加大嗎？)
+		//		    -> 你選了 false (不加大)。
+		//		    -> 所以 "大杯綠茶" (A) 出局，最後你拿到的是 "中杯綠茶" (B)。
+		// 結論：第一層的 true 只是讓你「有資格」去選 A 或 B，
+		//		       但最後是 A 還是 B，得看第二層的條件(true OR false)決定。
 		
 	}
 }
