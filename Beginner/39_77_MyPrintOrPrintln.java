c7e11288cdebf2b6616d4634df7d168a0c1f39c8
G120L010 <w25961941w@gmail.com>
2026-03-22 10:32:58 +0800

    Create 39_77_MyPrintOrPrintln.java

---
+++ Beginner/39_77_MyPrintOrPrintln.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class MyPrintOrPrintln {//練習輸出
+
+    public static void main(String[] args) {
+        // --- 情況 1：println ---
+        // 特色：印出文字後，會自動「換行」。
+        System.out.println("A"); // 印出 A，並換到下一行
+
+        // --- 情況 2：print ---
+        // 特色：印出文字後，不會換行，下一次輸出會接在同一行。
+        System.out.print("A"); // 印出 A，不換行
+        System.out.print("A"); // 再印出 A，接在前一個 A 後面
+    }
+}
