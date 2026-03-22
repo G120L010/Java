7bec45421e45a285c4d396171ab6ea16c870effd
G120L010 <w25961941w@gmail.com>
2026-03-22 10:34:22 +0800

    Update 39_77_MyPrintOrPrintln.java

--- Beginner/39_77_MyPrintOrPrintln.java
+++ Beginner/39_77_MyPrintOrPrintln.java
@@ -11,5 +11,8 @@ public class MyPrintOrPrintln {//練習輸出
         // 特色：印出文字後，不會換行，下一次輸出會接在同一行。
         System.out.print("A"); // 印出 A，不換行
         System.out.print("A"); // 再印出 A，接在前一個 A 後面
+
+        //println：印完 → 自動換行
+        //print：印完 → 不換行，接著印在同一行
     }
 }
