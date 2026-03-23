470404eff433f89493629895e1a3984431ef069d
G120L010 <w25961941w@gmail.com>
2026-03-23 15:21:29 +0800

    Create 61_121_MapSetDemo.java

---
+++ Advanced/61_121_MapSetDemo.java
@@ -0,0 +1,26 @@
+package demo4;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import demo.Dog;
+import demo.Duck;
+import demo.Fish;
+
+public class MapSetDemo {
+
+	public static void main(String[] args) {
+		Map map = new HashMap();
+		map.put("Dog", new Dog());// key-value："Dog"是key，而new Dog()是value。
+		map.put("Duck", new Duck());
+		map.put("Fish", new Fish());
+
+		System.out.println(map);
+		Set keySet = map.keySet();
+		for (Object object : keySet) {
+			System.out.println("key+" + object + ",value=" + map.get(object));
+		}
+	}
+
+}
