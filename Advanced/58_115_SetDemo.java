fc338b0aba09ac85fbf741744d9725d0bf645342
G120L010 <w25961941w@gmail.com>
2026-03-23 14:14:54 +0800

    Update 58_115_SetDemo.java

--- Advanced/58_115_SetDemo.java
+++ Advanced/58_115_SetDemo.java
@@ -1,24 +1,37 @@
 package demo4;
 
 import java.util.HashSet;
-import java.util.Set;
+// import java.util.Set;
+import java.util.TreeSet;
 
 public class SetDemo {
 
 	public static void main(String[] args) {
+		// TreeSet：自然排序
+		// new TreeSet();
+		// HashSet：雜湊表、紅黑樹的排序，效能較高
 		Set set = new HashSet();// 打完這行直接Ctrl + Shift + O，可直接選擇引入功能
 		int i = 1;
 		set.add("boy");
 		set.add("Dog");
 		set.add("Cat");
-		set.add(i);//這裡的i已經不是"八大基本資料型別"
-		System.out.println(set);
+		set.add(i);// 這裡的i已經不是"八大基本資料型別"
+		System.out.println("1.結果：" + set);
 
-		System.out.println(set.contains("Boy"));
-		System.out.println(set.contains("boy"));
+		System.out.println("2.找不到：" + set.contains("Boy"));
+		System.out.println("3.有找到：" + set.contains("boy"));
+
 		set.remove("boy");
-		System.out.println(set);
+		System.out.println("boy被移除：" + set);
 
+		System.out.println("資料長度(有幾筆)：" + set.size());
+
+		System.out.println("為空嗎？：" + set.isEmpty());// (is是不是)(Empty為空？)
+
+		set.clear();
+		System.out.println("清除clear：" + set);
+
+		System.out.println("為空嗎？：" + set.isEmpty());// (is是不是)(Empty為空？)
 	}
 
 }
