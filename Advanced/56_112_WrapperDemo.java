222b2966ddc92802a78530ab97379599b93d038d
G120L010 <w25961941w@gmail.com>
2026-03-23 11:45:31 +0800

    Create 56_112_WrapperDemo.java

---
+++ Advanced/56_112_WrapperDemo.java
@@ -0,0 +1,13 @@
+package demo4;
+
+public class WrapperDemo {//包裹資料型別
+
+	public static void main(String[] args) {
+		int i = 1;
+		Integer i2 = 1;//自動裝箱
+		Integer i3 = new Integer(1);
+		int i4 = i3;//自動拆箱
+
+	}
+
+}
