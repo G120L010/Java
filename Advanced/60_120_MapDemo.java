5957921d97d62dff0c1035d8cce1610a66c88c50
G120L010 <w25961941w@gmail.com>
2026-03-23 15:10:33 +0800

    Create 60_120_MapDemo.java

---
+++ Advanced/60_120_MapDemo.java
@@ -0,0 +1,27 @@
+package demo4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import demo.Dog;
+import demo.Duck;
+import demo.Fish;
+
+public class MapDemo {
+
+	public static void main(String[] args) {
+		Map map = new HashMap();
+		map.put("Dog", new Dog());// key-value："Dog"是key，而new Dog()是value。
+		map.put("Duck", new Duck());
+		map.put("Fish", new Fish());
+		System.out.println(map);
+		System.out.println(map.remove("Dog"));
+		System.out.println(map);
+		System.out.println(map.size());
+		map.clear();
+		System.out.println(map);
+		System.out.println(map.size());
+
+	}
+
+}
