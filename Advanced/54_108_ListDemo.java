42366cd0b0640fde5bbdefa617b963ee213e64b7
G120L010 <w25961941w@gmail.com>
2026-03-23 10:51:52 +0800

    Create 54_108_ListDemo.java

---
+++ Advanced/54_108_ListDemo.java
@@ -0,0 +1,18 @@
+package demo4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListDemo {
+
+	public static void main(String[] args) {
+		List list = new ArrayList();
+		list.add("Banana");
+		list.add("Apple");
+		list.add("Cat");
+		System.out.println(list);
+
+	}
+
+}
